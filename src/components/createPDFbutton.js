f72a0d272896dc9fe514fa90dd25b85e97479e66
RecepCelik1 <recep.celik@std.yildiz.edu.tr>
2024-04-30 00:44:09 +0300

    new-output

--- src/components/createPDFbutton.js
+++ src/components/createPDFbutton.js
@@ -1,8 +1,35 @@
 import React from "react";
-import jsPDF from 'jspdf';
-import 'jspdf-autotable';
+import { Page, Text, View, Document, PDFDownloadLink ,StyleSheet, Font, Svg, Line, Image} from '@react-pdf/renderer';
+import Gabarito from '../fonts/Gabarito/Gabarito-Regular.ttf'
+import GabaritoBold from '../fonts/Gabarito/Gabarito-Bold.ttf'
 import { useSelector } from "react-redux";
 
+Font.register({
+  family: 'Gabarito',
+  src : Gabarito,
+});
+Font.register({
+  family: 'GabaritoBold',
+  src : GabaritoBold,
+});
+
+const styles = StyleSheet.create({
+  page: {
+    display : "flex",
+    justifyContent : "center",
+    flexDirection: 'row',
+    paddingTop : "100px"
+  },
+  section: {
+    margin: 10,
+    padding: 15,
+    flexGrow: 1,
+    border : "1px",
+    borderColor : "gray",
+    maxWidth : "300px",
+    height:"150px"
+  }
+});
 
 const CreatePDFbutton = () => {
 
@@ -20,48 +47,59 @@ const CreatePDFbutton = () => {
     let state = informations.state
     let logo = informations.logo
 
-    const generatePDF = () => {
-
-      const doc = new jsPDF();
-
- 
-      doc.setDrawColor(169, 169, 169); 
-      doc.rect(45, 30, 125, 60, 'S');
-
-      if (logo) {
-        const img = new Image();
-        img.src = logo;
-        doc.addImage(img, 'PNG', 52, 37, 7, 7);
-        doc.addImage(img, 'JGP', 52, 37, 7, 7);
-        doc.addImage(img, 'JPEG', 52, 37, 7, 7);
-        console.log('Logo yüklendi');
-      }
-
-      doc.setFont("helvetica");
-      doc.setFontSize(10);
-      doc.text(`${companyName}`, 52 , 49)
-      doc.text(`${firstName + " " + lastName}`, 145 , 40.5)
-      doc.setFont("Times")
-      doc.text(`${jobTitle}`, 145 , 49)
-      doc.setDrawColor(169, 169, 169); // Gri renk (RGB)
-      doc.line(45, 59, 170, 59); 
-      doc.setFontSize(8);
-      doc.text(`${address}`, 52 , 69) 
-      doc.text(`${state}`, 52 , 75)
-      doc.text(`${city}`, 52 , 78)
-      doc.text(`${postalCode}`, 52 , 81)
-
-      doc.text(`${emailAdress}`, 122 , 78) 
-      doc.text(`${webSite}`, 122 , 81) 
-
-      doc.save('business-card.pdf'); 
-
+    const PDFile = () => {
+      return(
+        <Document>
+        <Page size="A4" style={styles.page}>
+          <View style={styles.section}>
+            <View style={{display : "flex", flexDirection : "row", justifyContent : "space-between"}}>
+              <View style={{display : "flex", justifyContent : "center", alignItems : "center"}}>
+                {logo !== null ? <Image src={logo} style={{width : "20px", height : "20px"}}/> : ""}
+              </View>
+              <View style={{display : "flex", justifyContent : "center", alignItems : "center"}}>
+                <Text style={{fontFamily : "GabaritoBold", fontSize : "8px", display : "flex", justifyContent : "center", alignItems : "center"}}>{firstName} {lastName}</Text>
+              </View>
+            </View>
+            <View style={{display : "flex", flexDirection : "row", justifyContent : "space-between", marginTop: "4px"}}>
+              <View style={{display : "flex", justifyContent : "center", alignItems : "center"}}>
+              <Text style={{fontFamily : "GabaritoBold", fontSize : "8px", display : "flex", justifyContent : "center", alignItems : "center"}}>{companyName}</Text>
+              </View>
+              <View style={{display : "flex", justifyContent : "center", alignItems : "center"}}>
+                <Text style={{fontFamily : "Gabarito", fontSize : "8px", display : "flex", justifyContent : "center", alignItems : "center"}}>{jobTitle}</Text>
+              </View>
+            </View>
+            <Svg style={{marginTop : '30px', marginBottom : '20px'}} height="10px" width="100%">
+            <Line
+              x1="0"
+              y1="2"
+              x2="350"
+              y2="2"
+              stroke="rgb(192,192,192)"
+            />
+          </Svg>
+          <View>
+            <Text style={{fontFamily : "Gabarito", fontSize : "8px"}}>{address}</Text>
+            <Text style={{fontFamily : "Gabarito", fontSize : "8px", marginTop : "8px"}}>{state}, {city}, {postalCode}</Text>
+            <Text style={{fontFamily : "Gabarito", fontSize : "8px", marginTop : "8px"}}>{country}</Text>
+          </View>
+          <View style={{marginTop : "8px"}}>
+            <Text style={{fontFamily : "Gabarito", fontSize : "8px", textAlign : "right"}}>{emailAdress}</Text>
+            <Text style={{fontFamily : "Gabarito", fontSize : "8px", marginTop : "8px", textAlign : "right"}}>{webSite}</Text>
+          </View>
+          </View>
+        </Page>
+      </Document>
+      )
     }
-
     return (
                  <div className='p-2'>
+                <div className="hidden">
+            <PDFDownloadLink document={<PDFile />} fileName="business-card.pdf" id="downloadPDF">
+            </PDFDownloadLink>
+          </div> 
+
                    <button 
-                   onClick={generatePDF}
+                   onClick={() => document.getElementById('downloadPDF').click()}
                    className='w-52 bg-sky-600 p-4 text-sm font-gabarito text-white rounded-md cursor-pointer hover:bg-sky-800 transition-all duration-200 ease-in-out'>Create Business Card</button>
                  </div>
     )
--- src/redux/informationSlice.js
+++ src/redux/informationSlice.js
@@ -1,7 +1,7 @@
     import { createSlice } from '@reduxjs/toolkit'
 
     const initialState = {
-        firstName : "alet balyoz gibi",
+        firstName : "",
         lastName : "",
         companyName : "",
         jobTitle : "",
